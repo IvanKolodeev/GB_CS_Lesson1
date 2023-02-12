74888932ef4ed5ce7dddf45a438deefc79a108f1
Ivan K <ikolodeev002@gmail.com>
2023-02-12 16:38:47 +0200

    Excersises done

---
+++ Excersise_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
---
+++ Excersise_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Even numbers are: ");
+
+for (int i = 0; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    }  
+}
